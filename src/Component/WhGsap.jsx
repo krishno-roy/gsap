d3e1efaf905d76a2e3a2ae0a62dc76ae5757f11d
krishnoroy <krishnorooy@gmail.com>
2025-05-01 21:24:40 +0600

    Animation add using Gsap

--- src/Component/WhGsap.jsx
+++ src/Component/WhGsap.jsx
@@ -1,22 +1,57 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 const WhGsap = () => {
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    const letters = textRef.current.querySelectorAll("span");
+
+    gsap.fromTo(
+      letters,
+      {
+        y: 50,
+        opacity: 0,
+        color: "#ffffff",
+      },
+      {
+        y: 0,
+        opacity: 1,
+        color: "#ffff",
+        stagger: {
+          each: 0.05,
+          from: "start",
+          ease: "sine.inOut",
+        },
+        duration: 0.8,
+        ease: "back.out(1.7)",
+      }
+    );
+  }, []);
+
+  const text =
+    "GSAP allows you to effortlessly animate anything JS can touch. Delivering silky-smooth performance and unmatched support so you can focus on the fun stuff.";
+
   return (
-    <div>
-      <div className="text-white container mx-auto py-7">
-        <h2 className="flex items-center text-2xl">
-          <span className="text-5xl ">{"{"}</span>
-          Why GSAP®
-          <span className="text-5xl">{"}"}</span>
-        </h2>
-        <h2 className='text-7xl leading-20 mt-10'>
-          GSAP allows you to effortlessly animate anything JS can touch.
-          Delivering silky-smooth performance and unmatched support so you can
-          focus on the fun stuff.
-        </h2>
-      </div>
+    <div className="text-white container mx-auto py-7">
+      <h2 className="flex items-center text-3xl">
+        <span className="text-5xl">{"{"}</span>
+        Why GSAP®
+        <span className="text-5xl">{"}"}</span>
+      </h2>
+
+      <h2
+        className="text-8xl leading-[6rem] mt-10 mb-10 flex flex-wrap"
+        ref={textRef}
+      >
+        {text.split("").map((char, i) => (
+          <span key={i} className="inline-block">
+            {char === " " ? "\u00A0" : char}
+          </span>
+        ))}
+      </h2>
     </div>
   );
-}
+};
 
-export default WhGsap
+export default WhGsap;
